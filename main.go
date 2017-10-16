93c3d36099edde7fdd1550a13429d24c034694c8
AbdelrahmanKhaledAmer <abdel-rahman.soliman@student.guc.edu.eg>
2017-10-16 13:13:10 +0200

    Added welcome route as well as JSON encoder (from Rami's ChatBot file)

--- main.go
+++ main.go
@@ -3,18 +3,30 @@ package main
 
 import (
 	"net/http"
+	"encoding/json"
 )
+
+type (
+	JSON map[string]interface{}
+)
+
+func writeJSON(w http.ResponseWriter, data JSON) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
 
 func main() {
 	http.HandleFunc("/", serve)
-	http.HandleFunc("/alaa", alaa)
+	http.HandleFunc("/welcome", startSession)
 	http.ListenAndServe(":8080", nil)
+}
+
+func startSession(w http.ResponseWriter, r *http.Request) {
+	writeJSON(w, JSON {
+		"message": "Welocme to GUC Carpool!",
+	})
 }
 
 func serve(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello world"))
-}
-
-func alaa(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello alaa"))
 }
