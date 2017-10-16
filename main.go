5a73cc2b0b94a3466e3851fe4680cc6ba017d28c
AbdelrahmanKhaledAmer <abdel-rahman.soliman@student.guc.edu.eg>
2017-10-16 13:57:15 +0200

    Started the chat function (Checking for POST requests only)

--- main.go
+++ main.go
@@ -4,29 +4,58 @@ package main
 import (
 	"net/http"
 	"encoding/json"
+	"log"
+	"net/http/httptest"
 )
 
 type (
 	JSON map[string]interface{}
+	//Session map[string]interface{}
 )
 
-func writeJSON(w http.ResponseWriter, data JSON) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+func writeJSON(res http.ResponseWriter, data JSON) {
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(data)
 }
 
 func main() {
-	http.HandleFunc("/", serve)
-	http.HandleFunc("/welcome", startSession)
+	http.HandleFunc("/", serveAndLog(serve))
+	http.HandleFunc("/welcome", serveAndLog(startSession))
+	http.HandleFunc("/chat", serveAndLog(chatBot))
 	http.ListenAndServe(":8080", nil)
 }
 
-func startSession(w http.ResponseWriter, r *http.Request) {
-	writeJSON(w, JSON {
+func serveAndLog(handler http.HandlerFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, req *http.Request) {
+		res := httptest.NewRecorder()
+		handler(res, req)
+		log.Printf("[%d] %-4s %s\n", res.Code, req.Method, req.URL.Path)
+
+		for k, v := range res.HeaderMap {
+			w.Header()[k] = v
+		}
+		w.WriteHeader(res.Code)
+		res.Body.WriteTo(w)
+	}
+}
+
+func startSession(res http.ResponseWriter, req *http.Request) {
+	// Ask about uuid
+	writeJSON(res, JSON {
 		"message": "Welocme to GUC Carpool!",
 	})
 }
 
-func serve(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world"))
+func chatBot(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		res.WriteHeader(http.StatusMethodNotAllowed)
+		writeJSON(res, JSON {
+			"message": "I'm sorry, but you didn't send and data with that GET request. I can only listen to POST requests on this route.",
+		})
+		return
+	}
+}
+
+func serve(res http.ResponseWriter, req *http.Request) {
+	res.Write([]byte("hello world"))
 }
